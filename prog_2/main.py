972e39207b27201009726f62cd9ca000560d50ab
RvJames26 <rovelljamesmariano05@gmail.com>
2026-04-24 23:17:08 +0800

    added class name and def __init

--- prog_2/main.py
+++ prog_2/main.py
@@ -1,15 +1,20 @@
-students = open("prog_2/students_gwa.txt", "r")
-highet_gwa = 5.00
-top_student = ""
+class students_gwa:
 
-for line in students:
-    data = line.split(",")
-    float_data = float(data[1])
-    
-    if float_data < highet_gwa:
-        highet_gwa = float_data
-        top_student = data[0]
+    def __init__(self, students, highest_gwa, top_student):
 
-print(f"The student who have the highest GWA is {top_student}, that have {highet_gwa}")
+        
+        students = open("prog_2/students_gwa.txt", "r")
+        highet_gwa = 5.00
+        top_student = ""
 
-students.close()
+        for line in students:
+            data = line.split(",")
+            float_data = float(data[1])
+            
+            if float_data < highet_gwa:
+                highet_gwa = float_data
+                top_student = data[0]
+
+        print(f"The student who have the highest GWA is {top_student}, that have {highet_gwa}")
+
+        students.close()
