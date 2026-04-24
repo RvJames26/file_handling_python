d470ef03f272a321e14ce1e6f0f7b8049f1b1c32
RvJames26 <rovelljamesmariano05@gmail.com>
2026-04-24 23:11:45 +0800

    added the if loop

--- prog_2/main.py
+++ prog_2/main.py
@@ -1,9 +1,16 @@
 students = open("prog_2/students_gwa.txt", "r")
+highet_gwa = 5.00
+top_student = ""
+
 for line in students:
     data = line.split(",")
     float_data = float(data[1])
-    print(data)
+    
+    if float_data < highet_gwa:
+        highet_gwa = float_data
+        top_student = data[0]
 
+print(top_student)
 
 
 
