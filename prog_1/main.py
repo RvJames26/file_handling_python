bbddff4b15f9d01e473a793fa27b92ab00c889d4
RvJames26 <rovelljamesmariano05@gmail.com>
2026-04-24 09:57:38 +0800

    succesfully created an even.txt and write all the even number inside

--- prog_1/main.py
+++ prog_1/main.py
@@ -1,13 +1,16 @@
 numbers = open("prog_1/numbers.txt", "r")
+even_num = open("prog_1/even.txt", "w")
 for line in numbers:
     num = int(line)
 
 #even
     if num % 2 == 0:
         print(num)
+        even_num.write(line)
 
 #odd
     if num % 2 == 1:
         print(num)
 
 numbers.close()
+even_num.close()
