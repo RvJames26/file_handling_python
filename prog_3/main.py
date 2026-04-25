f1b370c30c0c2c305153095d1db79c8b9b49b29b
RvJames26 <rovelljamesmariano05@gmail.com>
2026-04-25 08:41:01 +0800

    successfully append into a file

--- prog_3/main.py
+++ prog_3/main.py
@@ -2,12 +2,15 @@ my_life = open("prog_3/my_life.txt", "a")
 
 while True:
     line = input("Enter line: ")
+    my_life.write(line + "/n")
 
     yes_no = input("Are there more lines y/n? ")
     if yes_no == "y":
         continue
     if yes_no == "n":
         break
+
+    
     else:
         print("Invalid input, use only y and n")
 
