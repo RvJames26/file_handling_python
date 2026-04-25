1afbeb594253d4bc37b5613247344f42a1a63204
RvJames26 <rovelljamesmariano05@gmail.com>
2026-04-25 15:51:14 +0800

    used the import function to shorten the main.py

---
+++ prog_1/loop_function.py
@@ -0,0 +1,29 @@
+class NumberSort:
+
+    def __init__(self, even, odd, numbers):
+        self.even = even
+        self.odd = odd
+        self.numbers = numbers
+
+    def  process_file(self):
+        numbers = open("prog_1/numbers.txt", "r")
+        even_num = open("prog_1/even.txt", "w")
+        odd_num = open("prog_1/odd.txt", "w")
+        for line in numbers:
+            num = int(line)
+
+        #even
+            if num % 2 == 0:
+                print(num)
+                even_num.write(line)
+
+        #odd
+            if num % 2 == 1:
+                print(num)
+                odd_num.write(line)
+
+
+        numbers.close()
+        even_num.close()
+        odd_num.close()
+        print("Finish")
