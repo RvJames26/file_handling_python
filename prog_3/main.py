a7dbdec095466ca78bf65faad281762980ad2309
RvJames26 <rovelljamesmariano05@gmail.com>
2026-04-25 08:42:21 +0800

    successfully created the program, now making it into oop

--- prog_3/main.py
+++ prog_3/main.py
@@ -2,7 +2,7 @@ my_life = open("prog_3/my_life.txt", "a")
 
 while True:
     line = input("Enter line: ")
-    my_life.write(line + "/n")
+    my_life.write(line + "\n")
 
     yes_no = input("Are there more lines y/n? ")
     if yes_no == "y":
