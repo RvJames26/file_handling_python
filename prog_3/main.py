6f115337d53293d3c81c2bc4578a9c0699826f83
RvJames26 <rovelljamesmariano05@gmail.com>
2026-04-25 08:29:57 +0800

    added else if statement

---
+++ prog_3/main.py
@@ -0,0 +1,17 @@
+my_life = open("prog_3/my_life.txt", "a")
+
+while True:
+    line = input("Enter line: ")
+
+    yes_no = input("Are there more lines y/n? ")
+    if yes_no == y:
+        continue
+    if yes_no == n:
+        break
+    else:
+        print("Invalid input, use only y and n")
+
+    
+
+
+my_life.close()
