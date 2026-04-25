1e9c5feeafc45112d482d6bf4ccb1269536f4ca6
RvJames26 <rovelljamesmariano05@gmail.com>
2026-04-25 16:52:30 +0800

    created the writing in double.txt

--- prog_4/main.py
+++ prog_4/main.py
@@ -1,7 +1,14 @@
 numbers = open("prog_4/integers.txt", "r")
+square_even = open("prog_4/double.txt", "w")
+
 for line in numbers:
     num = int(line)
 
 #even
     if num % 2 == 0:
         square = num ** 2
+        print(square)
+        square_even.write(str(square) + "\n")
+
+numbers.close()
+square_even.close()
