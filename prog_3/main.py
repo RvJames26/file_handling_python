54e1173579f1496745ae06f94282d0e8f677e7c7
RvJames26 <rovelljamesmariano05@gmail.com>
2026-04-25 08:52:05 +0800

    created class name

--- prog_3/main.py
+++ prog_3/main.py
@@ -1,20 +1,22 @@
-my_life = open("prog_3/my_life.txt", "a")
+class life_file:
 
-while True:
-    line = input("Enter line: ")
-    my_life.write(line + "\n")
+    my_life = open("prog_3/my_life.txt", "a")
 
-    yes_no = input("Are there more lines y/n? ")
-    if yes_no == "y":
-        continue
-    if yes_no == "n":
-        break
+    while True:
+        line = input("Enter line: ")
+        my_life.write(line + "\n")
 
-    
-    else:
-        print("Invalid input, use only y and n")
+        yes_no = input("Are there more lines y/n? ")
+        if yes_no == "y":
+            continue
+        if yes_no == "n":
+            break
 
-    
+        
+        else:
+            print("Invalid input, use only y and n")
+
+        
 
 
-my_life.close()
+    my_life.close()
