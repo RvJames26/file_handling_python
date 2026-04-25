71ad20a9939d1b6aac231fb140190f1895955a2a
RvJames26 <rovelljamesmariano05@gmail.com>
2026-04-25 08:32:26 +0800

    fixed the if statement

--- prog_3/main.py
+++ prog_3/main.py
@@ -4,9 +4,9 @@ while True:
     line = input("Enter line: ")
 
     yes_no = input("Are there more lines y/n? ")
-    if yes_no == y:
+    if yes_no == "y":
         continue
-    if yes_no == n:
+    if yes_no == "n":
         break
     else:
         print("Invalid input, use only y and n")
