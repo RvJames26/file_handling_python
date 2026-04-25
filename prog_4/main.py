7082de3b065311dcc4444e8cd746622930043d74
RvJames26 <rovelljamesmariano05@gmail.com>
2026-04-25 17:00:02 +0800

    started making it into oop

--- prog_4/main.py
+++ prog_4/main.py
@@ -1,23 +1,25 @@
-numbers = open("prog_4/integers.txt", "r")
-square_even = open("prog_4/double.txt", "w")
-cube_odd = open("prog_4/triple.txt", "w")
+class Numbers:
 
-for line in numbers:
-    num = int(line)
+    numbers = open("prog_4/integers.txt", "r")
+    square_even = open("prog_4/double.txt", "w")
+    cube_odd = open("prog_4/triple.txt", "w")
 
-#even_square
-    if num % 2 == 0:
-        square = num ** 2
-        print(square)
-        square_even.write(str(square) + "\n")
+    for line in numbers:
+        num = int(line)
 
-#odd_cube
-    if num % 2 == 1:
-        cube = num ** 3
-        print(cube)
-        cube_odd.write(str(cube) + "\n")
+    #even_square
+        if num % 2 == 0:
+            square = num ** 2
+            print(square)
+            square_even.write(str(square) + "\n")
+
+    #odd_cube
+        if num % 2 == 1:
+            cube = num ** 3
+            print(cube)
+            cube_odd.write(str(cube) + "\n")
 
 
-numbers.close()
-square_even.close()
-cube_odd.close()
+    numbers.close()
+    square_even.close()
+    cube_odd.close()
