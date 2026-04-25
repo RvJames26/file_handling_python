92fb6b45214c0b5ca6eb4421d7c3b4716080de56
RvJames26 <rovelljamesmariano05@gmail.com>
2026-04-25 09:05:07 +0800

    added the read the file

--- prog_3/main.py
+++ prog_3/main.py
@@ -3,6 +3,7 @@ class life_file:
     def __init__(self, file_path):
         self.file_path = file_path
 
+    def writing_life(self):
         my_life = open("prog_3/my_life.txt", "a")
 
         while True:
@@ -14,6 +15,10 @@ class life_file:
                 continue
             if yes_no == "n":
                 break
+                read_file = open("prog_3/my_life.txt", "r")
+                for line in read_file:
+                    print(line)
+                    read_file.close()
 
             
             else:
