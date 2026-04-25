e58a48f728dfeba8d448e63d24530ae1c2c4f68d
RvJames26 <rovelljamesmariano05@gmail.com>
2026-04-25 16:59:12 +0800

    successfully make a if in odd and cube it and write it into the triple.txt

--- prog_4/main.py
+++ prog_4/main.py
@@ -1,14 +1,23 @@
 numbers = open("prog_4/integers.txt", "r")
 square_even = open("prog_4/double.txt", "w")
+cube_odd = open("prog_4/triple.txt", "w")
 
 for line in numbers:
     num = int(line)
 
-#even
+#even_square
     if num % 2 == 0:
         square = num ** 2
         print(square)
         square_even.write(str(square) + "\n")
 
+#odd_cube
+    if num % 2 == 1:
+        cube = num ** 3
+        print(cube)
+        cube_odd.write(str(cube) + "\n")
+
+
 numbers.close()
 square_even.close()
+cube_odd.close()
